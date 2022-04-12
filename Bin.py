cd3b42de1723fb9f5d50130a93144851cdff3cac
DreamZZCat <103490343+DreamZZCat@users.noreply.github.com>
2022-04-12 12:39:46 +0430

    Add files via upload

---
+++ Bin.py
@@ -0,0 +1,64 @@
+print("""                 
+                        ██████╗ ██╗███╗   ██╗
+                        ██╔══██╗██║████╗  ██║
+                        ██████╔╝██║██╔██╗ ██║
+                        ██╔══██╗██║██║╚██╗██║
+                        ██████╔╝██║██║ ╚████║
+                        ╚═════╝ ╚═╝╚═╝  ╚═══╝         
+ _________________________________________________________________
+| 1: spotify             | 2: paypal          | 3: discord_nitro  | 
+| 4: apple_music_3_month | 5: YOUTUBE_PREMIUM | 6: play_store     |
+| 7: HMA_VPN_30DAYS      | 8: ONLYFANS        | 9: INSTRAGRAM_ADS |
+| 10: TURBO_VPN          | 11: Prime_Gaming   | 12: SOUNDCLOUD    |
+| 13: PLAY_STATION_PLUS  | 14: DISNEY+        | 15: Amazon_Music  |
+|________________________|____________________|___________________|""")
+
+sad = input("|-->")
+
+    
+if int(sad) == 1:
+    #spotify
+    print(" BIN: 5268797472 | DATE: 08/26 | CVV: RND | ZIPCODE: 09101 | IP: Australia ")
+if int(sad) == 2:
+    #paypal
+    print(" BIN: 51546200 | IP: Usa ")
+if int(sad) == 3:
+    #discord_nitro
+    print("BIN: 545554 | FECHA/CVV: GENERADO | IP: USA | ZIPCODE: 10001")
+if int(sad) == 4:
+    #apple_music_3_month
+    print(" BIN: 4240930 | DATE: RND | CVV: RND | IP: INDIA ")
+if int(sad) == 5:
+    #YOUTUBE_PREMIUM
+    print("BIN: 52010603 | FECHA: RND | CVV: RND | IP: USA ")
+if int(sad) == 6:
+    #play_store
+    print("BIN: 3763920 | FECHA: RND | CVV: RND | IP: BRAZIL BR")
+if int(sad) == 7:
+    #HMA_VPN_30DAYS
+    print(" BIN: 4836980406 | FECHA: rnd | CVV: rnd | IP: USA | ZIP: 10080")
+if int(sad) == 8:
+    #ONLYFANS
+    print("BIN: 532655522777 | FECHA: 03/24 | CVV: RND | IP: CANADA | ZIP: A1A1A1")
+if int(sad) ==9:
+    #INSTRAGRAM_ADS
+    print("BIN: 454803526585 | FECHA: Generado | CVV: Generado | IP ESPAÑA  |Calle : sant joan d'en coll ,52 |Ciudad : Manresa |ZIP CODE: 08243")
+if int(sad) ==10:
+    #TURBO_VPN
+    print("BIN: 53915070508 | FECHA: RND | CVV: RND | ZIP CODE: 10080 | IP:  USA ")
+if int(sad) ==11:
+    #Prime_Gaming
+    print(" BIN: 417321528018 | FECHA: 03/27 | CVV: RND | IP: Own/just use Saudi Arabia")
+if int(sad) ==12:
+    #SOUNDCLOUD
+    print("BIN: 401658856104 | FECHA: 04/25 | CVV: RND | IP: CANADA ")
+if int(sad) ==13:
+    #PLAY_STATION_PLUS
+    print(" BIN: 511593 | FECHA: Generadon  CVV: Generado | IP : USA ")
+if int(sad) ==14:
+    #DISNEY+
+    print("Bin: 65015908XXXX100 | IP:USA | Zip Code: 10080")
+if int(sad) == 15:
+    #Amazon_Music
+    print("Bin : 4514051000266xxx | Fetcha : RND | CVV : RND | IP : Canadá ")
+input("--__ Enter To Close __--")
